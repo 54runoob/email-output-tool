263a545024920aaa3198399e381f5ee0f23ddbb8
zhaochi <1023313023@qq.com>
2017-12-30 01:46:38 +0800

    调整产出目录

--- index.js
+++ index.js
@@ -20,14 +20,40 @@ var suffixArr = ["126.com","sina.com","sohu.com","qq.com","163.com",
 function randomOutput(arr, len, total,suffixArr) {
     var temp = [];
     var reg = /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
+    var qqReg = /^[1-9]\d{4,8}@(\w)+((\.\w{2,3}){1,3})$/;
     for(var j=0;j<total;j++){
         var item = "";
         for(var i=0;i<len;i++){
             item = item + arr[Math.floor((Math.random()*arr.length))] + "";
         }
-        item += (tag + suffixArr[Math.floor((Math.random()*suffixArr.length))] + ""); 
+        item += (tag + suffixArr[Math.floor((Math.random()*suffixArr.length))] + "");
         temp.push(item)
         if (reg.test(item)) {
+            temp.push(item)
+        }
+    }
+    return temp;
+}
+
+/**
+ * QQ邮箱专属
+ * @param arr
+ * @param len
+ * @param total
+ * @param suffixArr
+ * @returns {Array}
+ */
+function randomQQOutput(arr, len, total,suffixArr) {
+    var temp = [];
+    var qqReg = /^[1-9]\d{4,8}@(\w)+((\.\w{2,3}){1,3})$/;
+    for(var j=0;j<total;j++){
+        var item = "";
+        for(var i=0;i<len;i++){
+            item = item + arr[Math.floor((Math.random()*arr.length))] + "";
+        }
+        item += (tag + suffixArr[Math.floor((Math.random()*suffixArr.length))] + "");
+        //temp.push(item)
+        if (qqReg.test(item)) {
             temp.push(item)
         }
     }
@@ -43,8 +69,8 @@ function randomOutput2(numberArr, numberCount,letterArr, letterCount, total, suf
         for(var i=0;i<(numberCount+letterCount);i++){
             item += (numberArr[Math.floor((Math.random()*numberArr.length))] + "");
         }
-    
-        item += (tag + suffixArr[Math.floor((Math.random()*suffixArr.length))] + ""); 
+
+        item += (tag + suffixArr[Math.floor((Math.random()*suffixArr.length))] + "");
         if (reg.test(item)) {
             temp.push(item)
         }
@@ -69,21 +95,30 @@ Array.prototype.uniqueTest = function() {
 // content =randomOutput2(numberArr.split(""), 6, letterArr.split(""), 1,10,suffixArr);
 //content = Array.from(test);
 sufArr = ["qq.com"];
-content = randomOutput(numberArr.split(""),6,999999,sufArr);
+var num = 1500000;
+var content0 = randomQQOutput(numberArr.split(""),5,num,sufArr);
+var content1 = randomQQOutput(numberArr.split(""),6,num,sufArr);
+var content2 = randomQQOutput(numberArr.split(""),7,num,sufArr);
+var content3 = randomQQOutput(numberArr.split(""),8,num,sufArr);
+var content4 = randomQQOutput(numberArr.split(""),9,num,sufArr);
+var content5 = randomQQOutput(numberArr.split(""),10,num,sufArr);
+var content6 = randomQQOutput(numberArr.split(""),11,num,sufArr);
+var content7 = randomQQOutput(numberArr.split(""),12,num,sufArr);
+content =content0.concat(content1).concat(content2).concat(content3).concat(content4).concat(content5).concat(content6).concat(content7);
 content = content.uniqueTest();
 console.log(content);
 function fileName(str, suffix){
     return str + "." + suffix;
 }
 console.log("该文件有" + content.length + "个邮箱");
-var filename = fileName("6位qq号","txt");
+var filename = fileName("5-12位qq号（"+ content.length +"个)","txt");
 /**
  * 写入文件
  */
-fs.writeFile("./" + filename, JSON.stringify(content), function(err) {
+fs.writeFile("./email/" + filename, JSON.stringify(content), function(err) {
     if(err) {
         return console.log(err);
     }
- 
+
     console.log("The file was saved!");
 });
